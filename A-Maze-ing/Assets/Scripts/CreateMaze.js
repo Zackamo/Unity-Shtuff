c81ac1510daa85fafa0e60a36f7a2a1936601ed6
Zackamo <zackmj99@gmail.com>
2015-03-22 12:26:54 -0500

    a 3

---
+++ A-Maze-ing/Assets/Scripts/CreateMaze.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+ 
+
+var barrier : Transform;
+
+function Create(type,x,y,z,rotation){
+	switch(type){
+		case "barrier":
+			Instantiate(barrier,new Vector3(x,y,z),rotation);
+			
+	}
+}
+
+function Start() { 
+
+Create("barrier",1,5,7,0);
+
+}
+
+function Update () {
+
+}
