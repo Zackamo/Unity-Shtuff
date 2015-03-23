4b89bdf6f9a336e3c059709aa3785a68fc1d3b0f
Zackamo <zackmj99@gmail.com>
2015-03-22 20:53:41 -0500

    b

--- A-Maze-ing/Assets/Scripts/CreateMaze.js
+++ A-Maze-ing/Assets/Scripts/CreateMaze.js
@@ -1,19 +1,52 @@
 ﻿#pragma strict
  
-
 var barrier : Transform;
+var path : Transform;
+var wall : Transform;
 
+//types are: "Straight", "Turn", "3Way", "4Way", "Gap", "Wall"
 function Create(type,x,y,z,rotation){
+			//create corner barriers and center path => exist in all types
+			Instantiate(barrier,new Vector3(x+1,y,z+1),barrier.rotation);
+			Instantiate(barrier,new Vector3(x+1,y,z-1),barrier.rotation);
+			Instantiate(barrier,new Vector3(x-1,y,z+1),barrier.rotation);
+			Instantiate(barrier,new Vector3(x-1,y,z-1),barrier.rotation);
+			Instantiate(path,new Vector3(x,y,z),path.rotation);
+	
+	barrier.rotation = Quaternion.AngleAxis(rotation, Vector3.up);
 	switch(type){
-		case "barrier":
-			Instantiate(barrier,new Vector3(x,y,z),rotation);
-			
+		case "Straight":
+			//straight along x-axis
+			if(rotation === 0){
+				Instantiate(barrier,new Vector3(x,y,z+1),barrier.rotation);
+				Instantiate(barrier,new Vector3(x,y,z-1),barrier.rotation);
+				Instantiate(path,new Vector3(x+1,y,z),path.rotation);
+				Instantiate(path,new Vector3(x-1,y,z),path.rotation);
+			}
+			//straight along z-axis
+			else{
+				Instantiate(barrier,new Vector3(x+1,y,z),barrier.rotation);
+				Instantiate(barrier,new Vector3(x-1,y,z),barrier.rotation);
+				Instantiate(path,new Vector3(x,y,z+1),path.rotation);
+				Instantiate(path,new Vector3(x,y,z-1),path.rotation);
+			}
+			break;
+		case "Turn":
+			break;
+		case "3Way":
+			break;
+		case "4Way":
+			break;
+		case "Gap":
+			break;
+		case "Wall":
+			break;			
 	}
 }
 
 function Start() { 
 
-Create("barrier",1,5,7,0);
+Create("Straight",0,0,0,0);
 
 }
 
---
+++ A-Maze-ing/Assets/Scripts/CreateMaze2.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var player : Transform;
+function Start() { 
+Instantiate(player,new Vector3(0,0,0),player.rotation);
+}
+
+function Update () {
+
+}
